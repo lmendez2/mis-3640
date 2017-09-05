586ea23aafd4767d0a267cd9283162efc10616dd
lmendez2 <lmendez2@babson.edu>
2017-09-05 14:50:31 -0400

    class work

---
+++ .vscode/Session 2/variable demo.py
@@ -0,0 +1,42 @@
+print('hello, world')
+
+print("hey", "jude" , "don\'t make it bad")
+
+print("46 + 37 + 38 =", 46+37+38)
+
+
+"""
+print('enter your name:')
+username = input()
+print(username)
+
+message = 'i learned something cool!~'
+"""
+
+ 
+pi = 3.14
+
+
+n = 100
+print(n + 100)
+
+first_name = 'lauren'
+last_name = 'mendez'
+
+fullname = first_name + " " + last_name
+print(fullname)
+
+print('hello', '%s' % 'world')
+
+
+print('today is %2d-%02d.' % (9,5))
+print('pi equals %.2f' % 3.1415926)
+print('age: %s. Gender %s' % (20, True))
+print('growth rate: %d %%' % 7)
+
+minutes = 45 # current time 
+
+# compute the percentage of the hour that has elapsed 
+percentage = (minutes * 100) / 60
+
+print(percentage)
