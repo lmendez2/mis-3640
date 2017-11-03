ce5deeac7864b24b1c1cd97f27acd39e52f632ba
lmendez2 <lmendez2@babson.edu>
2017-11-03 14:29:14 -0400

    stop_words
    doesnt work

--- text_mining-master/analysis_astros.py
+++ text_mining-master/analysis_astros.py
@@ -2,22 +2,23 @@ from twython import Twython
 import random 
 import string
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-# Replace the following strings with your own keys and secrets
-TOKEN = '926141030096875520-cPeB28AALjsDqXlrgZFfBP9UmmVJQsl'
-TOKEN_SECRET = 'v5d6uTaXnNYPHNjhgVJBeZ4A77NznKmHwl7qRh9zzvHmw'
-CONSUMER_KEY = '59CIo2eOMd7m8fjXVMHLBLjCY'
-CONSUMER_SECRET = '5tvo8vOrQqX4MYMOaMuiCbj5eBhVv06hIHKJtRMVK8iza77mCM'
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 
-"""t = Twython(CONSUMER_KEY, CONSUMER_SECRET,
-   TOKEN, TOKEN_SECRET)
+def process_data():
+    # Replace the following strings with your own keys and secrets
+    TOKEN = '926141030096875520-cPeB28AALjsDqXlrgZFfBP9UmmVJQsl'
+    TOKEN_SECRET = 'v5d6uTaXnNYPHNjhgVJBeZ4A77NznKmHwl7qRh9zzvHmw'
+    CONSUMER_KEY = '59CIo2eOMd7m8fjXVMHLBLjCY'
+    CONSUMER_SECRET = '5tvo8vOrQqX4MYMOaMuiCbj5eBhVv06hIHKJtRMVK8iza77mCM'
 
-data = t.search(q="Astros", count=50)
+    t = Twython(CONSUMER_KEY, CONSUMER_SECRET,
+    TOKEN, TOKEN_SECRET)
 
+    data = t.search(q="Astros", count=50)
+    return data
 
-for status in data['statuses']:
- print(status['text'])"""
 
 def process_file(data):
     """Makes a histogram that contains the words from a file.
@@ -27,16 +28,27 @@ def process_file(data):
     """
     hist = {}
 
-    for line in data:
-        line = line.replace("-", " ")
-        strippables = string.punctuation + string.whitespace
+    for status in data['statuses']:
+        line = status['text'].replace("-", " ")
+        strippables = string.punctuation + string.whitespace 
         for word in line.split():
             word = word.strip(strippables)
             word = word.lower()
             hist[word] = hist.get(word, 0) + 1 
     return hist
 
-def most_common(hist):
+def stop_words(hist):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(hist)
+
+    filtered_tweets = [w for w in word_tokenize if not w in stop_words]
+    hist_1 = []
+    for w in word_tokens: 
+        if w not in stop_words:
+            hist_1.append(w)
+    return hist_1
+
+def most_common(hist_1):
     """Makes a list of word-freq pairs in descending order of frequency.
     hist: map from word to frequency
     returns: list of (frequency, word) pairs
@@ -52,7 +64,7 @@ def most_common(hist):
 
 
 
-def print_most_common(hist, num=20):
+def print_most_common(hist_1, num=20):
     """Prints the most commons words in a histgram and their frequencies.
     hist: histogram (map from word to frequency)
     num: number of words to print
@@ -63,22 +75,16 @@ def print_most_common(hist, num=20):
         print(word, '\t', freq)
 
 def main():
-    t = Twython(CONSUMER_KEY , CONSUMER_SECRET , TOKEN , TOKEN_SECRET)
-    data = t.search(q="Astros", count=50)
-    
-    tweet_list = []
-    for status in data['statuses']:
-        tweet_list.append(status['text'])
+    data = process_data()
+    hist = process_file(data)
+    hist_1 = stop_words(hist)
+    print(hist_1)
 
-    hist = process_file(tweet_list)
-    print(hist)
-
-    t = most_common(hist)
-    print('The most common words are:')
+    t= most_common(hist_1)
+    print("The Most Common words are:")
     for freq, word in t[0:20]:
         print(word, '\t', freq)
 
-    words = process_file('text')
 
     sentence = "statuses"
     score = SentimentIntensityAnalyzer().polarity_scores(sentence)
